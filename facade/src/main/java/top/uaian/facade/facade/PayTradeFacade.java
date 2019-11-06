22344ba633ba97a2feeb98588f1a68cd8ecab900
xukainan <836127990@qq.com>
2019-11-06 10:21:32 +0800

    更改项目结构

--- facade/src/main/java/top/uaian/facade/facade/PayTradeFacade.java
+++
@@ -1,9 +0,0 @@
-package top.uaian.facade.facade;
-
-import top.uaian.facade.domain.PayRequestDto;
-import top.uaian.facade.domain.PayResponseDto;
-
-public interface PayTradeFacade {
-
-    public PayResponseDto payRequest(PayRequestDto payRequestDto);
-}
--- facade/src/main/java/top/uaian/facade/pay/Pay.java
+++
@@ -1,8 +0,0 @@
-package top.uaian.facade.pay;
-
-import top.uaian.facade.domain.PayRequestDto;
-import top.uaian.facade.domain.PayResponseDto;
-
-public interface Pay {
-     PayResponseDto pay();
-}
